122f43d14b0c0b7d378c07ebfabdf6ff7e6cdb83
isabelc <me@isabelcastillo.com>
2014-03-13 10:43:58 -0400

    Begin version control with Git. This is the last retail version.

---
+++ cpp-widget.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * Adds Current Planetary Positions Widget
+ *
+ * @author	Isabel Castillo
+ * @package 	Current Planetary Positions
+ * @extends 	WP_Widget
+ */
+
+class cpp_widget extends WP_Widget {
+	public function __construct() {
+		parent::__construct(
+	 		'cpp_widget',
+			__('Current Planetary Positions', 'cpp'),
+			array( 'description' => __( 'Display the current planetary positions in the zodiac signs.', 'cpp' ), )
+		);
+
+	}
+	public function isa_get_sign_position($longitude) {
+		$sym = array(aries,taurus,gemini,cancer,leo,virgo,libra,scorpio,sagittarius,capricorn,aquarius,pisces);
+
+		$localize_signs = array( __( 'Aries', 'cpp' ), __( 'Taurus', 'cpp' ), __( 'Gemini', 'cpp' ), __( 'Cancer', 'cpp' ), __( 'Leo', 'cpp' ), __( 'Virgo', 'cpp' ), __( 'Libra', 'cpp' ), __( 'Scorpio', 'cpp' ), __( 'Sagittarius', 'cpp' ), __( 'Capricorn', 'cpp' ), __( 'Aquarius', 'cpp'), __( 'Pisces', 'cpp') );
+
+
+		foreach ($sym as $key => $val) {
+			$symbol[$key] = '<span id="currentplanets_sprite" class="' . $val . '"></span> '. $localize_signs[$key];
+		}
+	
+		$sign_num = floor($longitude / 30);
+		$pos_in_sign = $longitude - ($sign_num * 30);
+		$deg = floor($pos_in_sign);
+		$full_min = ($pos_in_sign - $deg) * 60;
+		$min = floor($full_min);
+		$full_sec = round(($full_min - $min) * 60);
+		
+		$dms_numbers_range = range(0, 59);
+					
+		$localize_dms_numbers = array(_('00'),_('01'),_('02'),_('03'),_('04'),_('05'),_('06'),_('07'),_('08'),_('09'),_('10'),_('11'),_('12'),_('13'),_('14'),_('15'),_('16'),_('17'),_('18'),_('19'),_('20'),_('21'),_('22'),_('23'),_('24'),_('25'),_('26'),_('27'),_('28'),_('29'),_('30'),_('31'),_('32'),_('33'),_('34'),_('35'),_('36'),_('37'),_('38'),_('39'),_('40'),_('41'),_('42'),_('43'),_('44'),_('45'),_('46'),_('47'),_('48'),_('49'),_('50'),_('51'),_('52'),_('53'),_('54'),_('55'),_('56'),_('57'),_('58'),_('59'));
+					
+		$localized_dms = array_combine($dms_numbers_range,$localize_dms_numbers);
+	
+	
+		$localized_deg = $localized_dms[$deg];
+		$localized_min = $localized_dms[$min];
+		$localized_full_sec = $localized_dms[$full_sec];
+
+		// localize degree symbol
+
+		if( is_rtl() ) $degree =  "&deg;$localized_deg";
+		else $degree =  "$localized_deg&deg;";
+
+		$set_out = sprintf( __( '%s %s %s%s %s%s', 'cpp' ), 
+						$degree,
+						$symbol[$sign_num],
+						$localized_min,
+						chr(39),
+						$localized_full_sec,
+						chr(34)
+						);
+
+		return $set_out;
+
+	}
+	
+	
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+
+	public function widget( $args, $instance ) {
+
+		// get UT/GMT time for exec */
+		
+		$time = new DateTime('now', new DateTimeZone('UTC'));
+		
+		$utdate = $time->format('j').'.'.$time->format('n').'.'.$time->format('Y');// day.month.year (single-digit day, month, 4-digit month)
+		$uttime = $time->format('H').'.'.$time->format('i').'.'.$time->format('s');  // HH:MM:SS
+		
+		$num_planets = 11;
+		
+		$sweph = CPP_PLUGIN_DIR . 'sweph'; // set path to ephemeris
+		
+		unset($PATH,$out,$longitude,$speed);
+
+		$PATH = '';// WordPress is picky picky
+		
+		putenv("PATH=$PATH:$sweph");
+		
+		// get 11 planets
+		
+		exec ("isabelse -edir$sweph -b$utdate -ut$uttime -p0123456789D -eswe -fPls -g, -head", $out);
+		// output $row[] = (planet name, longitude decimal, speed)
+		// 1 row for each of 11 planets, indexed 0 - 10
+		
+		foreach ($out as $key => $line) {
+		
+		    $row = explode(',',$line);
+			$pl_name[$key] = $row[0];
+		    $longitude[$key] = $row[1];
+		    $speed[$key] = $row[2];
+		}
+		// localize planet names
+		$pl_name = array( __( 'Sun', 'cpp' ), __( 'Moon', 'cpp' ), __( 'Mercury', 'cpp' ), __( 'Venus', 'cpp' ), __( 'Mars', 'cpp' ), __( 'Jupiter', 'cpp' ), __( 'Saturn', 'cpp' ), __( 'Uranus', 'cpp' ), __( 'Neptune', 'cpp' ), __( 'Pluto', 'cpp' ), __( 'Chiron', 'cpp') );
+
+		extract( $args );
+		$title = apply_filters('widget_title', $instance['title']);
+		echo $before_widget;
+		if ( ! empty( $title ) )
+			echo '<h3 class="widget-title">'. $title . '</h3>';
+
+		// begin output to browser
+		echo '<div id="current-planets">';
+		
+		// display local date and time
+		echo '<p id="localtime">'; ?>
+		<script>
+			var d=new Date();
+			var n=d.toLocaleDateString();
+			var t=d.toLocaleTimeString(); 
+			document.write(n + "<br />" + t); 
+		</script>
+		<?php echo '</p>';
+		echo '<table>',"\n";
+		for ($i = 0; $i <= $num_planets - 1; $i++) {
+			echo '<tr><td>';
+				printf( __( '%s', 'cpp' ), $pl_name[$i] );
+				echo '&nbsp;';
+			echo '</td><td>';
+				echo $this->isa_get_sign_position($longitude[$i]);
+				if ($speed[$i] < 0) { //retrograde
+						echo '&nbsp;' . __('R', 'cpp' );
+				}
+			echo  '</td></tr>';
+		}
+		echo "</table></div>","\n";
+		
+		echo $after_widget;
+
+	}// end widget
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		return $instance;
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+
+		$defaults = array( 
+					'title' => __('Current Planetary Positions', 'cpp'),
+					);
+ 		$instance = wp_parse_args( (array) $instance, $defaults );
+    	?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
+				<?php _e( 'Title:', 'cpp' ); ?>
+			</label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" 
+				name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+		</p>
+		<?php 
+	}
+
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,149 @@
+﻿=== Current Planetary Positions Plugin for WordPress ===
+
+Author: Isabel Castillo
+Author URI: http://isabelcastillo.com
+License: GNU GPL Version 2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Current Planetary Positions is a WordPress plugin that adds a widget which displays the positions of the sun, the moon, Mercury, Venus, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto, and Chiron in the zodiac signs. From left to right, the columns display the planet's name, the degree of zodiac sign, zodiac sign symbol, zodiac sign text, the minute of degree, the second of degree, and retrograde motion (when indicated). If there appears an 'R' in the last column, then that planet is in apparent retrograde motion. The sun and moon are never retrograde. Please note: astrologers refer to all 11 of these celestial bodies as "planets" for the sake of convenience.
+
+The zodiac sign symbols are color coded according to their element: red for fire signs, green for earth, yellow for air, and blue for water. The widget also displays the current local date and time of the viewer, as given by the viewer's browser. 
+
+Current Planetary Positions uses the Swiss Ephemeris to get the longitude of the planets/celestial bodies. Information about the Swiss Ephemeris can be found at http://www.astro.com/swisseph/swephinfo_e.htm
+
+
+
+=== Languages ===
+
+These languages are included in the plugin:
+
+ - English (default)
+ - Arabic
+ - French
+ - Hindi
+ - Spanish
+
+This plugin is translation-ready, and includes a `.pot` file to make it easy for you to translate it into other languages.
+
+
+=== Installation ===
+
+1. Log in to you WordPress dashboard.
+2. Go to 'Plugins -> Add New'
+3. Click 'Upload', then upload the plugin file that you purchased.
+4. Activate the plugin by clicking "Activate".
+5. Now you are ready to use the plugin.
+6. The Current Planetary Positions widget will be available in `Appearance -> Widgets`
+7. To use the widget, drag the widget to a sidebar widget area like you would any other widget.
+
+
+=== Requirements ===
+
+WordPress 3.3.1+,  
+PHP 4.x+,  
+
+Note: The widget needs a minimum width of 250px to be displayed correctly. Unless, of course, you edit the CSS file to make the font-size smaller.
+
+
+
+=== CSS Selectors ===
+
+1. To style the entire widget, use this selector:
+
+`.widget_cpp_widget`
+
+2. To style the widget title, use this selector:
+
+`.widget_cpp_widget h3`
+
+3. To style the everything except the title, use this selector:
+
+`#current-planets`
+
+4. To style just the date, use this selector:
+
+`#current-planets #localtime`
+
+5. To style just the table of positions, use this selector:
+
+`#current-planets table`
+
+
+
+=== CSS Style Suggestions ===
+
+To center the widget title and the date and time, add this to `style.css`:
+
+`.widget_cpp_widget h3, #current-planets #localtime  {
+	text-align:center;
+}`
+
+
+
+=== Included Files ===
+
+cpp-widget.php
+current-planetary-positions.php
+EDD_SL_Plugin_Updater.php
+license.txt
+readme.txt
+style.css
+
+icons/icon-planets.png
+icons/sprites.png
+icons/sprites-mini.png
+
+languages/cpp.pot
+languages/cpp-ar.mo
+languages/cpp-ar.po
+languages/cpp-es_ES.mo
+languages/cpp-es_ES.po
+languages/cpp-fr_FR.mo
+languages/cpp-fr_FR.po
+languages/cpp-hi_IN.mo
+languages/cpp-hi_IN.po
+
+sweph/isabelse
+sweph/seas_18.se1
+sweph/semo_18.se1
+sweph/sepl_18.se1
+
+
+=== Support ===
+
+Documentation and Instruction Guides:
+
+http://isabelcastillo.com/docs/
+
+
+If you purchased this software at http://isabelcastillo.com, you can get support at http://isabelcastillo.com/support/
+
+
+== Changelog ==
+
+= 1.2.3 = 
+* New: plugin updates will now be available in WordPress dashboard.
+* New: localized numbers.
+* New: added French and Hindi language translations.
+* Maintenance: updated .pot file and all translation files.
+
+= 1.2.2 = 
+* New: added Spanish language and Arabic language translations.
+* New: added better support for rtl languages.
+* Tweak: added background transparency to icons.
+* Tweak: dynamic icons sizes to fit into thinner-width sidebars.
+* Maintenance: added CSS for Twenty Fourteen theme compatibility.
+* Maintenance: CSS tweaks: removed font color for compatibility with more themes.
+* Maintenance: updated .pot file.
+
+= 1.2.1 = 
+* Maintenance: removed _vti_ files.
+
+= 1.2 = 
+* New: use sprite map for icons.
+
+= 1.1 = 
+* New: set chmod automatically.
+
+= 1.0 =
+* Initial release of the WP plugin version.
